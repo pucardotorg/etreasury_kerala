d53d96f23766aeafaa669b03d8335416d50f79b3
Ajith-beehyv <ajith.k@beehyv.com>
2024-06-10 12:12:44 +0530

    logging request

--- webpage/app.js
+++ webpage/app.js
@@ -4,8 +4,17 @@ const bodyParser = require('body-parser');
 
 const app = express();
 const port = 8080;
-const externalHost = process.env.EXTERNAL_HOST || 'http://localhost:8080';
+const externalHost = process.env.EXTERNAL_HOST || 'http://localhost:8088';
 const contextPath = '/epayments';  
+
+//log incoming requests
+app.use((req, res, next) => {
+    console.log(`Incoming ${req.method} request to ${req.url}`);
+    if (req.method === 'POST' || req.method === 'PUT' || req.method === 'PATCH') {
+        console.log('Request body:', req.body);
+    }
+    next();
+});
 
 app.use(express.static('public'));
 app.use(express.json());
@@ -26,7 +35,7 @@ app.post(`${contextPath}`, (req, res) => {
     console.log(req.body);
 
     // Send a response with the appropriate HTML file
-    res.sendFile(path.join(__dirname, 'public', htmlFile));
+    //res.sendFile(path.join(__dirname, 'public', htmlFile));
 
 });
 
