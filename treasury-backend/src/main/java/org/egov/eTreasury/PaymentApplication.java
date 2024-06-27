25f7b949724c4c23a9671bb5d6796d15888b6990
Ajith-beehyv <ajith.k@beehyv.com>
2024-06-27 13:06:43 +0530

    removing tracer config from main class

--- treasury-backend/src/main/java/org/egov/eTreasury/PaymentApplication.java
+++ treasury-backend/src/main/java/org/egov/eTreasury/PaymentApplication.java
@@ -1,16 +1,12 @@
 package org.egov.eTreasury;
 
-import org.egov.tracer.config.TracerConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Primary;
 import org.springframework.web.client.RestTemplate;
 
 
-@Import({ TracerConfiguration.class })
 @SpringBootApplication
 public class PaymentApplication {
 
