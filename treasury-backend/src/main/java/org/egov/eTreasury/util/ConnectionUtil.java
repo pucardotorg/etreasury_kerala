9657274e1c1cdefc73067f5641ac96a359f3b5d2
Ajith-beehyv <ajith.k@beehyv.com>
2024-07-10 18:40:11 +0530

    remove body in httpentity

--- treasury-backend/src/main/java/org/egov/eTreasury/util/ConnectionUtil.java
+++ treasury-backend/src/main/java/org/egov/eTreasury/util/ConnectionUtil.java
@@ -6,7 +6,6 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
@@ -24,12 +23,12 @@ public class ConnectionUtil {
 
     public <T> ResponseEntity<T> callService(String url, Class<T> responseType) {
         HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
         List<MediaType> mediaTypeList = new ArrayList<>();
         mediaTypeList.add(MediaType.APPLICATION_JSON);
         headers.setAccept(mediaTypeList);
 
-        HttpEntity<MultiValueMap<String, String>> requestEntity = new HttpEntity<>(null, headers);
+        HttpEntity<Void> requestEntity = new HttpEntity<>(headers);
         return restTemplate.postForEntity(url, requestEntity, responseType);
     }
 }
