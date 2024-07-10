6818ce2fa274600aec18582e380fa05fe3c8efc8
Ajith-beehyv <ajith.k@beehyv.com>
2024-07-10 18:22:28 +0530

    added component annotation

--- treasury-backend/src/main/java/org/egov/eTreasury/util/ConnectionUtil.java
+++ treasury-backend/src/main/java/org/egov/eTreasury/util/ConnectionUtil.java
@@ -5,13 +5,14 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.stereotype.Component;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Component
 public class ConnectionUtil {
 
     private final RestTemplate restTemplate;
