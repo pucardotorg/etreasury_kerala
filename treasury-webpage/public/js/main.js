53d6127f649954d01f985b1c135995bf81901b75
shambhavigoel1002 <shambhavi.g@beehyv.com>
2024-07-11 11:20:12 +0530

    send response

--- treasury-webpage/public/js/main.js
+++ treasury-webpage/public/js/main.js
@@ -1,23 +1,33 @@
-const serverUrl = process.env.SERVER_URL || 'http://localhost:8090';
+const serverUrl = process.env.SERVER_URL || "http://localhost:8090";
 
-document.addEventListener('DOMContentLoaded', () => {
-    if (window.receivedData) {
+document.addEventListener("DOMContentLoaded", () => {
+  if (window.receivedData) {
+    const data = window.receivedData;
 
-        const data = window.receivedData;
-
-      /**  fetch(`${serverUrl}/v1/payment/challan_data`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
-        })
-        .then(response => response.json())
-        .then(data => {
-            console.log('Success:', data);
-        })
-        .catch((error) => {
-            console.error('Error:', error);
-        }); */
-    }
+    fetch(`${serverUrl}/payment/v1/_decryptTreasuryResponse`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Success:", data);
+        // Call function to handle the response
+        handleApiResponse(data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }
 });
+
+function handleApiResponse(responseData) {
+  window.apiResponseData = responseData;
+
+  const responseContainer = document.getElementById("response-container");
+  if (responseContainer) {
+    responseContainer.textContent = JSON.stringify(responseData, null, 2);
+  }
+}
