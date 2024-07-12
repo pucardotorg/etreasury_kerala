ea5e178de72a694aaa43b9fa482cb45620a19842
Ajith-beehyv <ajith.k@beehyv.com>
2024-07-12 16:46:07 +0530

    changing status to pending

--- treasury-backend/src/main/java/org/egov/eTreasury/model/TreasuryParams.java
+++ treasury-backend/src/main/java/org/egov/eTreasury/model/TreasuryParams.java
@@ -12,7 +12,7 @@ import lombok.Setter;
 public class TreasuryParams {
 
     private String authToken;
-    private String status;
+    private Boolean status;
     private String rek;
     private String hmac;
     private String data;
