9a47ef4d6d5d91a4f66cd36ae1e740bb7ed68b30
harshkumar8789 <harsh.kumar@beehyv.com>
2024-08-07 11:41:34 +0530

    commented some api and service

--- treasury-backend/src/main/java/org/egov/eTreasury/controller/PaymentController.java
+++ treasury-backend/src/main/java/org/egov/eTreasury/controller/PaymentController.java
@@ -43,14 +43,14 @@ public class PaymentController {
         return HtmlResponse.builder().payload(paymentPage).responseInfo(responseInfo).build();
     }
 
-    @PostMapping("/v1/_doubleVerification")
-    public HtmlResponse verifyDetails(@RequestBody VerificationRequest request) {
-        log.info("Performing double verification for request: {}", request);
-        Payload verificationPage = paymentService.doubleVerifyPayment(request.getVerificationData(), request.getRequestInfo());
-        ResponseInfo responseInfo = responseInfoFactory.createResponseInfoFromRequestInfo(request.getRequestInfo(), true);
-        log.info("Double verification successful for request: {}", request);
-        return HtmlResponse.builder().payload(verificationPage).responseInfo(responseInfo).build();
-    }
+//    @PostMapping("/v1/_doubleVerification")
+//    public HtmlResponse verifyDetails(@RequestBody VerificationRequest request) {
+//        log.info("Performing double verification for request: {}", request);
+//        Payload verificationPage = paymentService.doubleVerifyPayment(request.getVerificationData(), request.getRequestInfo());
+//        ResponseInfo responseInfo = responseInfoFactory.createResponseInfoFromRequestInfo(request.getRequestInfo(), true);
+//        log.info("Double verification successful for request: {}", request);
+//        return HtmlResponse.builder().payload(verificationPage).responseInfo(responseInfo).build();
+//    }
 
     @PostMapping("/v1/_printPayInSlip")
     public PrintResponse printPayInSlip(@RequestBody PrintRequest request) {
@@ -61,32 +61,32 @@ public class PaymentController {
         return PrintResponse.builder().responseInfo(responseInfo).document(document).build();
     }
 
-    @PostMapping("/v1/_transactionDetails")
-    public TransactionResponse processTransaction(@RequestBody TransactionRequest request) {
-        log.info("Fetching transaction details for request: {}", request);
-        TransactionDetails transactionDetails = paymentService.fetchTransactionDetails(request.getTransactionDetails(), request.getRequestInfo());
-        ResponseInfo responseInfo = responseInfoFactory.createResponseInfoFromRequestInfo(request.getRequestInfo(), true);
-        log.info("Transaction details fetched successfully for request: {}", request);
-        return TransactionResponse.builder().transactionDetails(transactionDetails).responseInfo(responseInfo).build();
-    }
+//    @PostMapping("/v1/_transactionDetails")
+//    public TransactionResponse processTransaction(@RequestBody TransactionRequest request) {
+//        log.info("Fetching transaction details for request: {}", request);
+//        TransactionDetails transactionDetails = paymentService.fetchTransactionDetails(request.getTransactionDetails(), request.getRequestInfo());
+//        ResponseInfo responseInfo = responseInfoFactory.createResponseInfoFromRequestInfo(request.getRequestInfo(), true);
+//        log.info("Transaction details fetched successfully for request: {}", request);
+//        return TransactionResponse.builder().transactionDetails(transactionDetails).responseInfo(responseInfo).build();
+//    }
 
-    @PostMapping("/v1/_refundPayment")
-    public RefundResponse processRefundPayment(@RequestBody RefundRequest request) {
-        log.info("Processing refund for request: {}", request);
-        ResponseInfo responseInfo = responseInfoFactory.createResponseInfoFromRequestInfo(request.getRequestInfo(), true);
-        RefundData refundData = paymentService.processRefund(request.getRefundDetails(), request.getRequestInfo());
-        log.info("Refund processed successfully for request: {}", request);
-        return RefundResponse.builder().responseInfo(responseInfo).refundData(refundData).build();
-    }
+//    @PostMapping("/v1/_refundPayment")
+//    public RefundResponse processRefundPayment(@RequestBody RefundRequest request) {
+//        log.info("Processing refund for request: {}", request);
+//        ResponseInfo responseInfo = responseInfoFactory.createResponseInfoFromRequestInfo(request.getRequestInfo(), true);
+//        RefundData refundData = paymentService.processRefund(request.getRefundDetails(), request.getRequestInfo());
+//        log.info("Refund processed successfully for request: {}", request);
+//        return RefundResponse.builder().responseInfo(responseInfo).refundData(refundData).build();
+//    }
 
-    @PostMapping("/v1/_refundStatus")
-    public RefundResponse checkRefundStatus(@RequestBody RefundStatusRequest request) {
-        log.info("Verifying refund status for request: {}", request);
-        ResponseInfo responseInfo = responseInfoFactory.createResponseInfoFromRequestInfo(request.getRequestInfo(), true);
-        RefundData refundData = paymentService.checkRefundStatus(request.getRefundStatus(), request.getRequestInfo());
-        log.info("Verified Refund Status successfully for request: {}", request);
-        return RefundResponse.builder().responseInfo(responseInfo).refundData(refundData).build();
-    }
+//    @PostMapping("/v1/_refundStatus")
+//    public RefundResponse checkRefundStatus(@RequestBody RefundStatusRequest request) {
+//        log.info("Verifying refund status for request: {}", request);
+//        ResponseInfo responseInfo = responseInfoFactory.createResponseInfoFromRequestInfo(request.getRequestInfo(), true);
+//        RefundData refundData = paymentService.checkRefundStatus(request.getRefundStatus(), request.getRequestInfo());
+//        log.info("Verified Refund Status successfully for request: {}", request);
+//        return RefundResponse.builder().responseInfo(responseInfo).refundData(refundData).build();
+//    }
 
     @PostMapping("/v1/_decryptTreasuryResponse")
     public ResponseEntity<ResponseInfo> decryptTreasuryResponse(@RequestBody TreasuryRequest request) {
