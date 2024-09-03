f0815ede16ad542ce0655f9725b4aa6ed7df108c
Ajithjaya <ajith30022@gmail.com>
2024-09-03 17:44:10 +0530

    node js for reading sbi push response and success , failure jsp pages

---
+++ sbi-webpage/app.js
@@ -0,0 +1,116 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios'); 
+const path = require('path');
+
+const app = express();
+const backendUrl = process.env.EXTERNAL_HOST || "http://localhost:8088/sbi-backend//v1/_decryptBrowserResponse";
+const pushResponseContextPath = "/sbi-payments";
+const successUrlContextPath = "/sbi-payment/success.jsp";
+const failUrlContextPath = "/sbi-payment/fail.jsp";
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+  async function callBackendService(backendUrl, data) {
+    try {
+      const requestInfo = await getRequestInfo();
+
+      const dataToSend = {
+        RequestInfo: requestInfo, 
+        encryptedPayload: JSON.stringify(data)
+      }
+
+      let backendResponse;
+      try {
+        // Log the data to send
+        console.log("Data to send:", JSON.stringify(dataToSend, null, 2));
+        backendResponse = await axios.post(`${backendUrl}`, dataToSend, {
+          headers: {
+            "Content-Type": "application/json",
+            "Authorization": "Basic ZWdvdi11c2VyLWNsaWVudDo="
+          }
+        });
+        console.log("Backend response:", backendResponse.data);
+      } catch (backendError) {
+        console.error("Backend request error:", backendError);
+        backendResponse = null;
+      }
+
+      console.log('Payload sent to backend successfully');
+    } catch (error) {
+      console.error('Error forwarding to backend:', error);
+      throw error;
+    }
+  }
+
+  function forwardJspPage(res, jspFileName) {
+    const jspFilePath = path.join(__dirname, "public", jspFileName);
+    res.sendFile(jspFilePath, (err) => {
+      if (err) {
+        console.error('Error serving JSP page:', err);
+        res.status(500).send('Error serving JSP page');
+      }
+    });
+  }
+
+  async function getRequestInfo() {
+    const url = process.env.DRISTI_URL || "https://dristi-kerala-dev.pucar.org/user/oauth/token?_=1713357247536";
+    const data = qs.stringify({
+      username: process.env.USERNAME || "sbi-payment-collector",
+      password: process.env.PASSWORD || "Dristi@123",
+      tenantId: "kl",
+      userType: "EMPLOYEE",
+      scope: "read",
+      grant_type: "password"
+    });
+  
+    const headers = {
+      'Cache-Control': 'no-cache',
+      'Connection': 'keep-alive',
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Authorization': 'Basic ZWdvdi11c2VyLWNsaWVudDo='
+    };
+  
+    try {
+      const response = await axios.post(url, data, { headers });
+      console.log("Response data:", response.data);
+  
+      const accessToken = response.data.access_token;
+      const userInfo = response.data.UserRequest;
+  
+      const requestInfo = {
+        apiId: "Rainmaker",
+        msgId: "1723548200333|en_IN",
+        authToken: accessToken,
+        userInfo: userInfo,
+        tenantId: "kl"
+      };
+  
+      return requestInfo;
+    } catch (error) {
+      console.error('Error fetching Auth token:', error.response ? error.response.data : error.message);
+      throw error;
+    }
+  }
+
+  app.post(`${successUrlContextPath}`, (req, res) => {
+    console.log('Request body:', JSON.stringify(req));
+    callBackendService( backendUrl, req.body);
+    forwardJspPage(res, '/success.jsp');
+  });
+  
+  app.post(`${failUrlContextPath}`, (req, res) => {
+    console.log('Request body:', JSON.stringify(req));
+    callBackendService( backendUrl, req.body);
+    forwardJspPage(res, '/fail.jsp');
+  });
+
+  app.post(`${pushResponseContextPath}`, async (req, res) => {
+    console.log('Request body:', JSON.stringify(req));
+    callBackendService( backendUrl, req.body);
+  });
+
+app.listen(port, () => {
+  console.log(`sbi payments app listening on port ${port}`);
+});
