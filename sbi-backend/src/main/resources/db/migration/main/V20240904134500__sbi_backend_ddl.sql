dc9032b21293c811eca833e886b797a083afc89e
Ajithjaya <ajith30022@gmail.com>
2024-09-04 11:15:36 +0530

    updated tenant id type in migration file

--- sbi-backend/src/main/resources/db/migration/main/V20240904134500__sbi_backend_ddl.sql
+++ sbi-backend/src/main/resources/db/migration/main/V20240904134500__sbi_backend_ddl.sql
@@ -5,5 +5,5 @@ ALTER TABLE transaction_details
 DROP COLUMN IF EXISTS fail_url;
 
 ALTER TABLE transaction_details
-ADD COLUMN IF NOT EXISTS tenant_id;
+ADD COLUMN IF NOT EXISTS tenant_id VARCHAR(30);
 
